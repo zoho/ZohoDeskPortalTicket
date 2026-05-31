dcc1e6a6b6c36b8e11f174f4a54e75fecde5a59d
Vinoth N <vinoth.nv@zohocorp.com>
2026-05-31 18:15:32 +0530

    4.6.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = Version("4.6.2")
+let version = Version("4.6.3")
 let package = Package(
     name: "ZohoDeskPortalTicket",
     platforms: [
@@ -13,22 +13,21 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZohoDeskPortalTicket",
-            targets: ["ZohoDeskPortalTicket", "ZohoDeskPortalTicketPackage"]),
+            targets: ["ZohoDeskPortalTicket", "ZDLayoutKit", "ZohoDeskPortalTicketPackage"]),
     ],
     dependencies: [
         .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: version),
-        .package(url: "https://github.com/zoho/ZDLayoutKit.git", exact: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ZohoDeskPortalTicket", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalTicket/\(version.description)/ZohoDeskPortalTicket.zip", checksum: "3031a1e8de147cb1bbb356dbe0af42fccf2e15927db14aaa14740b4a12b773d2"),
+        .binaryTarget(name: "ZohoDeskPortalTicket", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalTicket/\(version.description)/ZohoDeskPortalTicket.zip", checksum: "bd7a60b1889449c2f3f8f64c4c6ab81381b5265420c54f58f41064b8eed05427"),
+        .binaryTarget(name: "ZDLayoutKit", url: "https://maven.zohodl.com/ZohoDesk/ZDLayoutKit/1.1.0/ZDLayoutKit.zip", checksum: "1970583b624961004150c549a007c1b856fc9cd16dea866ec1c5744b1b5885ef"),
         
         .target(
             name: "ZohoDeskPortalTicketPackage",
             dependencies: [
-                .product(name: "ZohoDeskPortalConfiguration", package: "ZohoDeskPortalConfiguration"),
-                .product(name: "ZDLayoutKit", package: "ZDLayoutKit"),
+                .product(name: "ZohoDeskPortalConfiguration", package: "ZohoDeskPortalConfiguration")
             ]
         )
     ]
