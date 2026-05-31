7800f25b02cff30e122b0efdc6af6c9c6a0e1370
Vinoth N <vinoth.nv@zohocorp.com>
2026-05-31 18:28:09 +0530

    AttachmentUploader dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZohoDeskPortalTicket",
-            targets: ["ZohoDeskPortalTicket", "ZDLayoutKit", "ZohoDeskPortalTicketPackage"]),
+            targets: ["ZohoDeskPortalTicket", "ZDLayoutKit", "ZDAttachmentUploader", "ZohoDeskPortalTicketPackage"]),
     ],
     dependencies: [
         .package(url: "https://github.com/zoho/ZohoDeskPortalConfiguration.git", exact: version),
@@ -23,6 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ZohoDeskPortalTicket", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalTicket/\(version.description)/ZohoDeskPortalTicket.zip", checksum: "bd7a60b1889449c2f3f8f64c4c6ab81381b5265420c54f58f41064b8eed05427"),
         .binaryTarget(name: "ZDLayoutKit", url: "https://maven.zohodl.com/ZohoDesk/ZDLayoutKit/1.1.0/ZDLayoutKit.zip", checksum: "1970583b624961004150c549a007c1b856fc9cd16dea866ec1c5744b1b5885ef"),
+        .binaryTarget(name: "ZDAttachmentUploader", url: "https://maven.zohodl.com/ZohoDesk/ZDAttachmentUploader/1.1.0/ZDAttachmentUploader.zip", checksum: "0ae4b175a50bb4522d3a3d2f9a5e24a0685d3bac03ccca03492eea3cd3561af9"),
         
         .target(
             name: "ZohoDeskPortalTicketPackage",
